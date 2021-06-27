a10c0b0e54ac9466851b92a464e0b5cc37cf40b4
dzikimlecz <j.pul312@gmail.com>
2021-06-27 20:06:54 +0200

    added timer for controlling flow of the game

---
+++ src/main/java/me/dzkimlecz/snake/controller/Timer.java
@@ -0,0 +1,35 @@
+package me.dzkimlecz.snake.controller;
+
+import me.dzkimlecz.snake.game.GameBoard;
+import me.dzkimlecz.snake.game.Snake;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+public class Timer {
+    private final ScheduledExecutorService executor;
+    private final Snake snake;
+    private final GameBoard board;
+
+    public Timer(Snake snake, GameBoard board) {
+        this.snake = snake;
+        this.board = board;
+        executor = Executors.newSingleThreadScheduledExecutor();
+    }
+
+    public void start() {
+        if (executor.isShutdown())
+            throw new IllegalStateException("Can't use the same timer object twice");
+
+        executor.scheduleAtFixedRate(() -> {
+            board.tick();
+            snake.move();
+        }, 200, 200, MILLISECONDS);
+    }
+
+    public void stop() {
+        executor.shutdownNow();
+    }
+}
