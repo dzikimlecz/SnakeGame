d6a2aa14b14691ca1c54dd802865c3a506b19dff
dzikimlecz <j.pul312@gmail.com>
2021-06-27 20:05:30 +0200

    added application launcher

---
+++ src/main/java/me/dzkimlecz/snake/Launcher.java
@@ -0,0 +1,10 @@
+package me.dzkimlecz.snake;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(SnakeGame.class);
+    }
+}
+
---
+++ src/main/java/me/dzkimlecz/snake/SnakeGame.java
@@ -0,0 +1,39 @@
+package me.dzkimlecz.snake;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import me.dzkimlecz.snake.components.BoardView;
+import me.dzkimlecz.snake.controller.Timer;
+import me.dzkimlecz.snake.game.GameBoard;
+import me.dzkimlecz.snake.game.Snake;
+
+public class SnakeGame extends Application {
+//    private Timer timer;
+//    private GameBoard board;
+//    private Snake snake;
+//    private BoardView boardView;
+
+    @Override
+    public void start(Stage primaryStage) {
+//        final var root = new BorderPane();
+//        root.setCenter(boardView = new BoardView());
+//        final var topBar = new VBox();
+//        // TODO: 27.06.2021
+//        root.setTop(topBar);
+//        primaryStage.setScene(new Scene(root));
+//        primaryStage.sizeToScene();
+//        primaryStage.show();
+    }
+
+    public void startGame() {
+//        timer = new Timer(snake = new Snake(), board = new GameBoard(15));
+//        boardView.bind(board);
+//        timer.start();
+    }
+
+}
