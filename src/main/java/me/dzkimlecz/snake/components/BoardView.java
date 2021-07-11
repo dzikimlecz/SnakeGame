78c4ed784b08aabf68c0367819a36ef087428cd0
dzikimlecz <j.pul312@gmail.com>
2021-07-11 21:30:50 +0200

    implemented component used for displaying the board

--- src/main/java/me/dzkimlecz/snake/components/BoardView.java
+++ src/main/java/me/dzkimlecz/snake/components/BoardView.java
@@ -1,18 +1,49 @@
 package me.dzkimlecz.snake.components;
 
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
 import me.dzkimlecz.snake.game.GameBoard;
 
-public class BoardView extends Pane {
+import static javafx.scene.paint.Color.*;
 
-    private GameBoard board;
+public class BoardView extends StackPane {
+
+    private final GridPane displayPane;
 
     public BoardView() {
         super();
         // TODO: 27.06.2021  
+        displayPane = new GridPane();
+        super.getChildren().add(displayPane);
     }
 
     public void bind(GameBoard board) {
-        this.board = board;
+        displayPane.getChildren().clear();
+        final var size = board.size();
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
+                final var rectangle = new Rectangle();
+                displayPane.add(rectangle, column, row);
+                board.squareStateProperty(column, row).addListener((obs, oldVal, newVal) -> {
+                    switch (newVal) {
+                        case EMPTY:
+                            rectangle.setFill(LIGHTGREY);
+                            break;
+                        case SNAKE_BODY:
+                            rectangle.setFill(GREEN);
+                            break;
+                        case SNAKE_HEAD:
+                            rectangle.setFill(DARKGREEN);
+                            break;
+                        case APPLE:
+                            rectangle.setFill(RED);
+                            break;
+                        default:
+                            throw new AssertionError();
+                    }
+                });
+            }
+        }
     }
 }
