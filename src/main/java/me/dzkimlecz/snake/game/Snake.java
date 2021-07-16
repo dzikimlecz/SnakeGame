f4f72c1d9d72bfd83263e803d783c462164d8b02
dzikimlecz <j.pul312@gmail.com>
2021-07-16 23:06:09 +0200

    implemented movement of the snake

--- src/main/java/me/dzkimlecz/snake/game/Snake.java
+++ src/main/java/me/dzkimlecz/snake/game/Snake.java
@@ -1,7 +1,47 @@
 package me.dzkimlecz.snake.game;
 
+import me.dzkimlecz.snake.util.Pair;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class Snake {
+    private final Deque<Pair<Integer>> body;
+    private final AtomicReference<Direction> direction;
+
+    public Snake() {
+        direction = new AtomicReference<>();
+        body = new LinkedList<>();
+    }
+
+    private void turn(Direction direction) {
+        if (!direction.isOppositeTo(this.direction.get()))
+            this.direction.set(direction);
+    }
+
     public void move() {
-        // TODO: 27.06.2021  
+        body.removeLast();
+        final var direction = this.direction.get();
+        final Pair<Integer> delta;
+        switch (direction) {
+            case UP:
+                delta = new Pair<>(0, -1);
+                break;
+            case DOWN:
+                delta = new Pair<>(0, 1);
+                break;
+            case LEFT:
+                delta = new Pair<>(-1, 0);
+                break;
+            case RIGHT:
+                delta = new Pair<>(1, 0);
+                break;
+            default:
+                throw new AssertionError();
+        }
+        final var head = body.getFirst();
+        final var newHead = new Pair<>(head.first() + delta.first(), head.second() + delta.second());
+        body.addFirst(newHead);
     }
 }
