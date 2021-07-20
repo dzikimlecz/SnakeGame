cc433e72e754cf22c28501702a6e3d0568b8120b
dzikimlecz <j.pul312@gmail.com>
2021-07-20 19:46:37 +0200

    mocked snake steering

--- src/main/java/me/dzkimlecz/snake/SnakeGame.java
+++ src/main/java/me/dzkimlecz/snake/SnakeGame.java
@@ -6,10 +6,13 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import me.dzkimlecz.snake.components.BoardView;
+import me.dzkimlecz.snake.controller.SnakeSteering;
 import me.dzkimlecz.snake.controller.Timer;
 import me.dzkimlecz.snake.game.GameBoard;
 import me.dzkimlecz.snake.game.Snake;
 import me.dzkimlecz.snake.util.Pair;
+
+import static me.dzkimlecz.snake.controller.GameEvent.*;
 
 public class SnakeGame extends Application {
     private Timer timer;
@@ -30,10 +33,20 @@ public class SnakeGame extends Application {
     }
 
     public void startGame() {
-        timer = new Timer(snake = new Snake(Pair.of(7, 7)), board = new GameBoard(15));
+        var snake = new Snake(Pair.of(7, 7));
+        var board = new GameBoard(15);
+        var timer = new Timer(snake, board);
+        var steering = new SnakeSteering(snake, () -> {
+            final var location = snake.headLocation();
+            if (location.first() == 14) return TURN_BOTTOM;
+            else if (location.first() == 1) return TURN_TOP;
+            else if (location.second() == 14) return TURN_LEFT;
+            else if (location.second() == 1) return TURN_RIGHT;
+            else return null;
+        });
         boardView.bind(board);
         timer.run();
-//        steering.run();
+        steering.run();
     }
 
 }
